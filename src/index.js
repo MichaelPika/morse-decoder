18d210ec4a7db5d4d556b49d2747b033dbd71935
MichaelPika <xauru7@gmail.com>
2022-01-30 00:41:07 +0200

    feat:add decoder

--- src/index.js
+++ src/index.js
@@ -38,8 +38,47 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let lettersArr = [];
+    let coderArr = [];
+    let coderValue = "";
+    let result = [];
+
+    for (let i = 0; i < expr.length; i += 10) {
+      lettersArr.push(expr.slice(i, i+10));
+    }
+
+    lettersArr.forEach(element => {
+       for(let i = 0; i < element.length; ){
+          if(element[i] == 1 && element[i + 1] == 1){
+           coderValue = coderValue + "-";
+            i = i + 2;
+         } else if(element[i] == 1 && element[i + 1] == 0){
+           coderValue = coderValue + ".";
+           i = i + 2;
+         } else if(element[i] == "*"){
+           coderValue = " ";
+           i = 10;
+         } else{
+           i++;
+         }
+       }
+      coderArr.push(coderValue);
+      coderValue = "";
+    });
+    
+    for(let i = 0; i < coderArr.length; i++){
+      if(coderArr[i] == " "){
+        result.push(" ");
+      }
+      for(let key in MORSE_TABLE){
+        if(coderArr[i] == key){
+          result.push(MORSE_TABLE[key]);
+        }
+      }
+    }
+    
+    return(result.join(""));
+  }
 
 module.exports = {
     decode
